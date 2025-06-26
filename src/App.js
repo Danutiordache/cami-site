03d53801696ea4e3d8d0741a856571d0405a77a6
Danutiordache <danutiordache2001@gmail.com>
2025-06-26 09:21:25 +0300

    Actualizare poza Cami

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 export default function CamiSite() {
-  const pozaURL = "https://i.postimg.cc/ydxLJmgW/Whats-App-Image-2025-06-26-at-08-12-44-1.jpg";
-  const citat = "Fiecare zi e o șansă nouă să iubești, să ierți, să simți. — Cami";
+  const pozaURL = "https://i.postimg.cc/qMJZD5p9/Whats-App-Image-2025-06-26-at-09-17-37.jpg";
+  const citat = "apa curge siroaie ,aprins ca o vapaie ,
+ca doi chinezi cu ochii mici mancam la doua paie";
 
   return (
     <main className="min-h-screen bg-gradient-to-br from-pink-100 via-rose-100 to-yellow-100 flex flex-col items-center justify-center p-6 text-center font-serif">
